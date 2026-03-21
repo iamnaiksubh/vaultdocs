d0958c9d901b3a31fe86947c0264cca5a1d95b3f
iamnaiksubh <subhashishnaik1@gmail.com>
2026-03-21 12:09:13 +0530

    initial commit

---
+++ src/test/java/com/vaultdocs/VaultdocsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vaultdocs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VaultdocsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
